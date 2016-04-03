e1b0460f3747fbfa8fba067b6c444d10d650390c
Abhi <suriabhinav1997@gmail.com>
2016-04-03 08:18:22 -0400

    heroku

--- config/auth.js
+++ config/auth.js
@@ -2,11 +2,11 @@ module.exports = {
   'facebookAuth' : {
     'clientID' : '1678010332461062',
     'clientSecret' : '114b1a1c5e992a9aa723c10b4e411aef',
-    'callbackURL' : 'http://localhost:8080/auth/facebook/callback'
+    'callbackURL' : 'http://facebot.tech/auth/facebook/callback'
   },
   'twitterAuth' : {
       'consumerKey'       : 'xzWiUTlSulJAlfnMbI16X60Wk',
       'consumerSecret'    : 'pEKMP5ZuNHPuoPF44xLC1IBkgUvFLosZPsSAPVD1XJhFnXwTpd',
-      'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+      'callbackURL'       : 'http://192.155.89.214:8080/auth/twitter/callback'
   },
 };
