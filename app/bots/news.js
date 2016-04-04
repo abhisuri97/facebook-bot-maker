fc22ccf329c8f9ef166cf3fefcf0600d9b059a04
Jared Winograd <jarwino@gmail.com>
2016-04-04 18:38:24 -0400

    Remove interval

--- app/bots/news.js
+++ app/bots/news.js
@@ -1,9 +1,7 @@
 var GoogleNews, googleNews, track;
 
 var GoogleNews = require('google-news');
-var googleNews = new GoogleNews({
-  pollInterval: Number.MAX_VALUE
-});
+var googleNews = new GoogleNews();
 
 module.exports = function(query, cb) {
   googleNews.stream(query, function(stream) {
