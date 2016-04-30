9ed95afa612133ca9a52f39ff17824c25d63d3c4
Abhi <suriabhinav1997@gmail.com>
2016-04-30 16:50:11 -0400

    added dummy config

---
+++ config/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+  'facebookAuth' : {
+    'clientID' : 'FACEBOOK_CLIENT_ID_HERE',
+    'clientSecret' : 'FACEBOOK_CLIENT_SECRET_HERE',
+    'callbackURL' : 'http://DOMAIN_NAME/auth/facebook/callback'
+  },
+  'twitterAuth' : {
+      'consumerKey'       : 'TWITTER_CONSUMER_KEY',
+      'consumerSecret'    : 'TWITTER_CONSUMER_SECRET',
+      'callbackURL'       : 'http://DOMAIN_NAME/auth/twitter/callback'
+  },
+  'watson' : {
+    'alchemy' : 'WATSON_ALCHEMY_API_KEY',
+  }
+};
